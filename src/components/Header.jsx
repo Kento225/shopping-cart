377a9fd8ece09f2c4b91648e574574e604905139
Kento225 <augustaskiela899@gmail.com>
2023-08-04 16:07:31 +0300

    Fix cart icon rendering

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import shoppingCartIcon from "../assets/shopping-cart-outline-svgrepo-com.svg";
 
 export function Header({ cartCount }) {
   return (
@@ -13,10 +14,7 @@ export function Header({ cartCount }) {
       <div className="right-side">
         <Link to="/CartPage">
           <p className="cart-count">{cartCount}</p>
-          <img
-            src="../src/assets/shopping-cart-outline-svgrepo-com.svg"
-            className="cart-icon"
-          ></img>
+          <img src={shoppingCartIcon} className="cart-icon"></img>
         </Link>
       </div>
     </div>
